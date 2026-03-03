4bdcf7dc8e7d427b43448cb4282ed7d27e92380c
Son <2355010173@kientruchanoi.edu.vn>
2026-03-03 11:38:34 +0700

    feat: add legacy plugin support and update build target in Vite configuration

--- apps/client/vite.config.js
+++ apps/client/vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'Safari >= 12'],
+      modernPolyfills: true,
+    }),
+  ],
   server: {
     port: 5173,
     proxy: {
